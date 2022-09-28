215e3c142fd2dc87e58c6831bc1e16157fc129e2
Andrew0T <andrewtimmins@internode.on.net>
2022-09-28 17:59:44 +0200

    Update scripts.js
    Deleted old script from task 1.1 and new script required for task 1.2. Comments also made.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello World');
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+// alert('Hello World');
+//let favoriteFood = 'Pasta';
+//document.write(favoriteFood); old task 1.1 deleted as required to start next task.
+// new task for 1.2 started
+let pokemonList = [
+	{ name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+	{ name: 'Dewgong', height: 17, types: ['ice', 'water'] },
+	{ name: 'Zapdos', height: 12, types: ['electic', 'flying'] }
+];
+console.log( [pokemonList])
