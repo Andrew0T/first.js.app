89e9db8cdce697026890767989e033976525eae1
Andrew0T <andrewtimmins@internode.on.net>
2022-10-05 11:41:53 +0200

    Update scripts.js
    Added updates from exericise 1.3 to add document.write, conditional to pokemonList.
    Stop JS from reading exercise 1.1 & 1.2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,41 @@
+// exericise 1.1
 // alert('Hello World');
 //let favoriteFood = 'Pasta';
 //document.write(favoriteFood); old task 1.1 deleted as required to start next task.
 // new task for 1.2 started
+
+// exercise 1.2 pokomon name list in an array with types in another array.
+//let pokemonList = [
+//  { name: 'Zapdos', height: 1.7, types: ['electic', 'flying']
+//},
+//{ name: 'Seel', height: 1.1, types: ['ice', 'water']
+//},
+//{ name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']
+//}
+//];
+// exercise 1.3 the 'for' loop highlighting the name, height and types of the pokemon
 let pokemonList = [
-	{ name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-	{ name: 'Dewgong', height: 17, types: ['ice', 'water'] },
-	{ name: 'Zapdos', height: 12, types: ['electic', 'flying'] }
+  { name: 'Zapdos',
+    height: 1.7, 
+   types: ['electic', 'flying']
+  },
+  {
+   name: 'Seel',
+   height: 1.1,
+    types: ['ice', 'water']
+  },
+  { name: 'Bulbasaur',
+   height: 0.7, 
+   types: ['grass', 'poison']
+  }
 ];
-console.log( [pokemonList])
+let i = 0;
+for (let i = 0; i < 3; i++) {
+    if (pokemonList[i].height >= 1.2) {
+      document.write(pokemonList[i].name + "is" + pokemonList[i].height + " meters tall" + "Wow, that is big!" + pokemonList[i].types)
+  } else if (pokemonList[i].height < 1.2 && pokemonList[i].height > 0.8) {
+      document.write(pokemonList[i].name + "is" + pokemonList[i].height + " meters tall" + pokemonList[i].types)
+  } else if (pokemonList[i].height < 0.8) {
+      document.write(pokemonList[i].name + "is" + pokemonList[i].height + " meters tall" + pokemonList[i].types)
+  }
+};
